31ea582d641c84c3e4fd91e518de48db71e886e2
ZerZoul <a.adnot@gmail.com>
2021-05-11 11:09:40 +0200

    Navbar compoents created

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav className="app-navbar">
+            <ul className="app-nav-link">
+                <li><a href="#rules">Les règles</a></li>
+                <li><a href="#cities">Les villes</a></li>
+                <li><a href="#objects">Objets</a></li>
+                <li><a href="#question">F.A.Q</a></li>
+            </ul>
+        </nav>
+    );
+}
+ 
+export default NavBar;
